edd7aece1e8db8a58b3d587045596391e6dcbe87
Ovidiu Liuta <info@thinkovi.com>
2017-02-16 13:47:16 +0200

    updating phar

--- generate-restore-script.sh
+++ generate-restore-script.sh
@@ -2,5 +2,5 @@
 #cp -rf vendor tmp/
 #cp restore/xcloner_restore.php.txt tmp/xcloner_restore.php
 /Applications/MAMP/bin/php/php7.0.12/bin/php phar-generate.php
-tar -czpf restore/vendor.tgz vendor/
+#tar -czpf restore/vendor.tgz vendor/
 #rm -rf tmp/*
