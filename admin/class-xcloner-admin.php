f695c18c9304416e1987f4c1216d9ae41427142b
Ovidiu Liuta <info@thinkovi.com>
2017-02-24 15:41:29 +0200

    path fixes temp

--- admin/class-xcloner-admin.php
+++ admin/class-xcloner-admin.php
@@ -142,6 +142,13 @@ class Xcloner_Admin {
 	
 	public function xcloner_scheduled_backups_page()
 	{
+		$requirements 			= new XCloner_Requirements();
+		if(!$requirements->check_backup_ready_status())
+		{
+			require_once("partials/xcloner_init_page.php");
+			return false;
+		}
+		
 		require_once("partials/xcloner_scheduled_backups_page.php");
 		
 	}
