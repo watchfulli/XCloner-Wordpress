e72bb75819c6c5033a3b4dd05291686be20ba772
Ovidiu Liuta <info@thinkovi.com>
2017-02-27 09:54:16 +0200

    backup list fix

--- includes/class-xcloner-file-system.php
+++ includes/class-xcloner-file-system.php
@@ -24,7 +24,7 @@ class Xcloner_File_System{
 	private $files_size;
 	private $last_logged_file;
 	private $folders_to_process_per_session = 25;
-	private $backup_archive_extensions = array("zip", "tar", "tgz", "tar.gz", "gz", "csv");
+	private $backup_archive_extensions = array("tar", "tgz", "tar.gz", "gz", "csv");
 	private $backup_name_tags = array('[time]', '[hostname]', '[domain]');
 	
 	public function __construct($hash = "")
