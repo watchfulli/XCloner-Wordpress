abf8e2ae68a0068bc802b80fd27adec3796cd06c
Ovidiu Liuta <info@thinkovi.com>
2017-03-03 12:02:16 +0200

    js scrutinizer code fix

--- admin/js/xcloner-remote-storage-class.js
+++ admin/js/xcloner-remote-storage-class.js
@@ -12,7 +12,9 @@ class Xcloner_Remote_Storage
 		var field = jQuery(elem).attr("name")
 		var value = 0
 		if(jQuery(elem).is(":checked"))
+		{
 			value = 1;
+		}
 			
 		if(field){
 			jQuery.ajax({
@@ -21,7 +23,9 @@ class Xcloner_Remote_Storage
 			  data: { action : 'remote_storage_save_status', id: field, value: value},
 			  success: function(response){
 				  if(!response.finished)
-					alert('Error changing status')
+					{
+						alert('Error changing status')
+					}
 				  },
 			  dataType: 'json'
 			  
