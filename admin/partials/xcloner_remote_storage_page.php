adacc0eff3c39f72ce3a7c9cd5d6cb94e339cf0e
Ovidiu Liuta <info@thinkovi.com>
2017-03-20 07:08:50 +0200

    sftp private key fix

--- admin/partials/xcloner_remote_storage_page.php
+++ admin/partials/xcloner_remote_storage_page.php
@@ -184,8 +184,7 @@ $gdrive_construct = $remote_storage->gdrive_construct();
 							<label for="sftp_private_key"><?php echo __("SFTP Private Key(RSA)",'xcloner-backup-and-restore')?></label>
 						</div>
 						<div class=" col s12 m6">
-							<textarea rows="5" placeholder="<?php echo __("Local Server Path or Contents of the SFTP Private Key RSA File",'xcloner-backup-and-restore')?>" id="sftp_private_key" type="text" name="xcloner_sftp_private_key" class="validate" value=""><?php echo get_option("xcloner_sftp_private_key")?>
-							</textarea>
+							<textarea rows="5" placeholder="<?php echo __("Local Server Path or Contents of the SFTP Private Key RSA File",'xcloner-backup-and-restore')?>" id="sftp_private_key" type="text" name="xcloner_sftp_private_key" class="validate" value=""><?php echo get_option("xcloner_sftp_private_key")?></textarea>
 						</div>
 			        </div>
 			        
