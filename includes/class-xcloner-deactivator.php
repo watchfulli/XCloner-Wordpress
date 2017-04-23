abb2e828d4d4565044510fe2891cc8e242001208
Ovidiu Liuta <info@thinkovi.com>
2017-04-23 10:41:27 +0300

    deactivate exception handling errors

--- includes/class-xcloner-deactivator.php
+++ includes/class-xcloner-deactivator.php
@@ -35,7 +35,12 @@ class Xcloner_Deactivator {
 		
 		if(is_a($xcloner_plugin, 'Xcloner'))
 		{
-			$xcloner_plugin->get_xcloner_filesystem()->cleanup_tmp_directories();
+			try{
+				$xcloner_plugin->get_xcloner_filesystem()->cleanup_tmp_directories();
+			}catch(Exception $e)
+			{
+				$xcloner_plugin->trigger_message_notice($e->getMessage());
+			}
 			
 			$xcloner_scheduler = $xcloner_plugin->get_xcloner_scheduler();
 			$xcloner_scheduler->deactivate_wp_cron_hooks();
