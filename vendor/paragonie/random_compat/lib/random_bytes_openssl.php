65ee059a9cc20b5a286bce1a802cb41cc903b442
Ovidiu Liuta <info@thinkovi.com>
2018-05-19 09:14:54 +0300

    venrdor/paragonie update

--- vendor/paragonie/random_compat/lib/random_bytes_openssl.php
+++ vendor/paragonie/random_compat/lib/random_bytes_openssl.php
@@ -39,52 +39,48 @@
  * 
  * @return string
  */
- 
-if(!function_exists('random_bytes')) 
+function random_bytes($bytes)
 {
-	function random_bytes($bytes)
-	{
-	    try {
-	        $bytes = RandomCompat_intval($bytes);
-	    } catch (TypeError $ex) {
-	        throw new TypeError(
-	            'random_bytes(): $bytes must be an integer'
-	        );
-	    }
-	
-	    if ($bytes < 1) {
-	        throw new Error(
-	            'Length must be greater than 0'
-	        );
-	    }
-	
-	    /**
-	     * $secure is passed by reference. If it's set to false, fail. Note
-	     * that this will only return false if this function fails to return
-	     * any data.
-	     * 
-	     * @ref https://github.com/paragonie/random_compat/issues/6#issuecomment-119564973
-	     */
-	    $secure = true;
-	    /**
-	     * @var string
-	     */
-	    $buf = openssl_random_pseudo_bytes($bytes, $secure);
-	    if (
-	        is_string($buf)
-	        &&
-	        $secure
-	        &&
-	        RandomCompat_strlen($buf) === $bytes
-	    ) {
-	        return $buf;
-	    }
-	
-	    /**
-	     * If we reach here, PHP has failed us.
-	     */
-	    throw new Exception(
-	        'Could not gather sufficient random data'
-	    );
-	}
+    try {
+        $bytes = RandomCompat_intval($bytes);
+    } catch (TypeError $ex) {
+        throw new TypeError(
+            'random_bytes(): $bytes must be an integer'
+        );
+    }
+
+    if ($bytes < 1) {
+        throw new Error(
+            'Length must be greater than 0'
+        );
+    }
+
+    /**
+     * $secure is passed by reference. If it's set to false, fail. Note
+     * that this will only return false if this function fails to return
+     * any data.
+     * 
+     * @ref https://github.com/paragonie/random_compat/issues/6#issuecomment-119564973
+     */
+    $secure = true;
+    /**
+     * @var string
+     */
+    $buf = openssl_random_pseudo_bytes($bytes, $secure);
+    if (
+        is_string($buf)
+        &&
+        $secure
+        &&
+        RandomCompat_strlen($buf) === $bytes
+    ) {
+        return $buf;
+    }
+
+    /**
+     * If we reach here, PHP has failed us.
+     */
+    throw new Exception(
+        'Could not gather sufficient random data'
+    );
 }
