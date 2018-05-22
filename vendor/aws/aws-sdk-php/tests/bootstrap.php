29a4767bc5470b016f18c3a1cef939602895816b
Ovidiu Liuta <info@thinkovi.com>
2018-05-22 13:25:03 +0300

    vendor cleanup

--- vendor/aws/aws-sdk-php/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(-1);
-date_default_timezone_set('UTC');
-
-// Include the composer autoloader
-$loader = require __DIR__ . '/../vendor/autoload.php';
-
-// Clear out any previously compiled JMESPath files.
-JmesPath\Env::cleanCompileDir();
