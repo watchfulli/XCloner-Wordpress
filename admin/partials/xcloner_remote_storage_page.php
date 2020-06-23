97efa8d8633de645920ea75ce380906ec8706112
Ovidiu Liuta <info@thinkovi.com>
2020-06-23 17:13:10 +0300

    #102 onedrive support addon code

--- admin/partials/xcloner_remote_storage_page.php
+++ admin/partials/xcloner_remote_storage_page.php
@@ -16,7 +16,7 @@ function common_cleanup_html($type)
     if ($type == "local") {
         $type = "";
     }else{
-        $type .= $type."_";
+        $type .= "_";
     }
     ob_start(); ?>
 <!-- Cleanup by Days -->
@@ -617,6 +617,80 @@ $common_cleanup_html = ob_get_contents();
                             <div class="col s6 m4">
                                 <button class="btn waves-effect waves-light orange" type="submit" name="action"
                                         id="action" value="dropbox"
+                                        onclick="jQuery('#connection_check').val('1')"><?php echo __("Verify", 'xcloner-backup-and-restore') ?>
+                                    <i class="material-icons right">import_export</i>
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+                </li>
+
+                <!-- ONEDRIVE STORAGE-->
+                <li id="onedrive">
+                    <div class="collapsible-header">
+                        <i class="material-icons">computer</i><?php echo __("OneDrive Storage", 'xcloner-backup-and-restore') ?>
+                        <div class="switch right">
+                            <label>
+                                Off
+                                <input type="checkbox" name="xcloner_onedrive_enable" class="status"
+                                       value="1" <?php if (get_option("xcloner_onedrive_enable")) {
+                                        echo "checked";
+                                    } ?> \>
+                                <span class="lever"></span>
+                                On
+                            </label>
+                        </div>
+                    </div>
+                    <div class="collapsible-body">
+
+                        <div class="row">
+                            <div class="col s12 m3 label">
+                                &nbsp;
+                            </div>
+                            <div class=" col s12 m6">
+                                <p>
+									<?php echo sprintf(__('Visit %s and get your "Api Key".', 'xcloner-backup-and-restore'), '<a href="https://azure.microsoft.com/en-us/services/storage/blobs/" target="_blank">https://azure.microsoft.com/en-us/services/storage/blobs/</a>') ?>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col s12 m3 label">
+                                <label for="onedrive_client_id"><?php echo __("OneDrive Cliend ID", 'xcloner-backup-and-restore') ?></label>
+                            </div>
+                            <div class=" col s12 m6">
+                                <input placeholder="<?php echo __("OneDrive Client ID", 'xcloner-backup-and-restore') ?>"
+                                       id="onedrive_client_id" type="text" name="xcloner_onedrive_client_id" class="validate"
+                                       value="<?=get_option("xcloner_onedrive_client_id") ?>"
+                                       autocomplete="off">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col s12 m3 label">
+                                <label for="onedrive_client_secret"><?php echo __("OneDrive Cliend Secret", 'xcloner-backup-and-restore') ?></label>
+                            </div>
+                            <div class=" col s12 m6">
+                                <input placeholder="<?php echo __("OneDrive Client Secret", 'xcloner-backup-and-restore') ?>"
+                                       id="onedrive_client_secret" type="password" name="xcloner_onedrive_client_secret" class="validate"
+                                       value="<?=get_option("xcloner_onedrive_client_secret") ?>"
+                                       autocomplete="off">
+                            </div>
+                        </div>
+
+                        <?=common_cleanup_html('onedrive')?>
+
+                        <div class="row">
+                            <div class="col s6 m4">
+                                <button class="btn waves-effect waves-light" type="submit" name="action" id="action"
+                                        value="onedrive"><?php echo __("Save Settings", 'xcloner-backup-and-restore') ?>
+                                    <i class="material-icons right">save</i>
+                                </button>
+                            </div>
+                            <div class="col s6 m4">
+                                <button class="btn waves-effect waves-light orange" type="submit" name="action"
+                                        id="action" value="onedrive"
                                         onclick="jQuery('#connection_check').val('1')"><?php echo __("Verify", 'xcloner-backup-and-restore') ?>
                                     <i class="material-icons right">import_export</i>
                                 </button>
