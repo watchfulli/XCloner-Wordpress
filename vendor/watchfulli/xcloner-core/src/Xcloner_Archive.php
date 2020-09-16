720b270ce2b532e14e4356e678801754b9bfaf38
Ovidiu Liuta <info@thinkovi.com>
2020-09-16 10:57:25 +0300

    #110 site url fix to match get_home_url

--- vendor/watchfulli/xcloner-core/src/Xcloner_Archive.php
+++ vendor/watchfulli/xcloner-core/src/Xcloner_Archive.php
@@ -313,7 +313,7 @@ class Xcloner_Archive extends Tar
         $body .= "<br />";
 
         $body .= sprintf(__("Backup Site Url: %s"), get_site_url());
-        echo $body .= "<br />";exit;
+        $body .= "<br />";
 
         if (isset($params['backup_params']->backup_comments)) {
             $body .= __("Backup Comments: ").$params['backup_params']->backup_comments;
