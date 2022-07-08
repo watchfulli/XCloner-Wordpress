8083065a7c993025582c0aa2641f8644aeb1c8aa
Aris Ongaro <mail@arisongaro.it>
2022-07-08 13:29:36 +0200

    chore: review folders structure

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,27 +1,19 @@
 // Webpack uses this to work with directories
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
-const MergeIntoSingleFilePlugin = require("webpack-merge-and-include-globally");
 
 // This is the main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
   // Path to your entry point. From this file Webpack will begin his work
-  //entry: './admin/js/index.js',
   entry: {
-    index: "./admin/js/index.js",
-    /*xcloner_admin: "./admin/js/xcloner-admin.js",
-    xcloner_backup_class: "./admin/js/xcloner-backup-class.js",
-    xcloner_manage_backups_class: "./admin/js/xcloner-manage-backups-class.js",
-    xcloner_remote_storage_class: "./admin/js/xcloner-remote-storage-class.js",
-    xcloner_restore_class: "./admin/js/xcloner-restore-class.js",
-    xcloner_scheduler_class: "./admin/js/xcloner-scheduler-class.js",*/
+    index: "./xcloner-backup-and-restore/admin/js/index.js",
   },
 
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "xcloner-backup-and-restore/dist"),
     filename: "./admin/js/[name].min.js",
   },
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,22 +6,14 @@ const TerserPlugin = require("terser-webpack-plugin");
 // Here you write different options and tell Webpack what to do
 module.exports = {
   // Path to your entry point. From this file Webpack will begin his work
-  //entry: './admin/js/index.js',
   entry: {
-    index: "./admin/js/index.js",
-    /*xcloner_admin: "./admin/js/xcloner-admin.js",
-    xcloner_backup_class: "./admin/js/xcloner-backup-class.js",
-    xcloner_manage_backups_class: "./admin/js/xcloner-manage-backups-class.js",
-    xcloner_remote_storage_class: "./admin/js/xcloner-remote-storage-class.js",
-    xcloner_restore_class: "./admin/js/xcloner-restore-class.js",
-    xcloner_scheduler_class: "./admin/js/xcloner-scheduler-class.js",
-    */
+    index: "./xcloner-backup-and-restore/admin/js/index.js",
   },
 
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "xcloner-backup-and-restore/dist"),
     filename: "./admin/js/[name].min.js",
   },
 
@@ -29,7 +21,7 @@ module.exports = {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
-  
+
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
   // on final bundle. For now we don't need production's JavaScript
